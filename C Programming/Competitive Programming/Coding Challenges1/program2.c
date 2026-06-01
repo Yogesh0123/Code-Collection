38fcd0982a434faf4080a06cb42865a89dbd84a5
Yogesh Anand Gadhave <111746500+Yogesh0123@users.noreply.github.com>
2026-06-01 14:34:40 +0530

    C Programming Competitive Files uploded

---
+++ C Programming/Competitive Programming/Coding Challenges1/program2.c
@@ -0,0 +1,22 @@
+//2.Program to print 5 times “Marvellous” on screen.
+//Output: Marvellous
+//        Marvellous
+//        Marvellous
+//        Marvellous
+//        Marvellous
+#include<stdio.h>
+
+void Display()
+{
+    int i =0 ;
+    for (i = 1; i<=5;i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+int main()
+{
+    Display();
+
+    return 0;
+}
