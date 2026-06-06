df1ccc298e0da454595c19d28abbc312dd302bdc
Yogesh Anand Gadhave <111746500+Yogesh0123@users.noreply.github.com>
2026-06-06 19:41:51 +0530

    C Programming Competitive Files Uploaded

---
+++ C Programming/Competitive Programming/Coding Challenges 3/program4.c
@@ -0,0 +1,30 @@
+//4.Accept one character from user and convert case of that character.
+//Input : a Output : A
+//Input : D Output : d
+
+#include <stdio.h>
+
+void DisplayConvert (char CValue)
+{
+    if ((CValue >= 'a')&&(CValue <= 'z'))
+    {
+        printf("%c",CValue-32);
+    }
+    else if ((CValue >= 'A')&& (CValue <= 'Z'))
+    {
+        printf("%c",CValue +32);
+    }
+    
+    
+}
+
+int main()
+{
+    char cValue ='\0';
+
+    printf("Enter Character \n");
+    scanf("%c",&cValue);
+
+    DisplayConvert(cValue);
+    return 0;
+}
