25d04a57ce97a8ce92cd4129dea178ef4483ffb5
Yogesh Anand Gadhave <111746500+Yogesh0123@users.noreply.github.com>
2026-06-16 16:15:41 +0530

    C Programming Competitive Files Uploded

---
+++ C Programming/Competitive Programming/Coding Challenges 10/program4.c
@@ -0,0 +1,58 @@
+/*
+4.Write a program which accept range from user and return addition of all even
+numbers in between that range. (Range should contains positive numbers only)
+Input : 23 30
+Output : 108
+Input : 10 18
+Output : 70
+Input : -10 2
+Output : Invalid range
+Input : 90 18
+Output : Invalid range
+*/
+
+#include<stdio.h>
+
+    int RangeSumEven(int iStart, int iEnd)
+    {
+        int Sum = 0;
+        if ((iStart < 0 )||(iStart <0) || (iStart > iEnd) )
+        {
+            return -1;
+        }
+        while (iStart <= iEnd)
+        {
+            if (iStart % 2 ==0)
+            {
+                Sum = Sum+iStart;
+            }
+            iStart++;
+            
+        }
+        return Sum;
+    }
+
+int main()
+{
+    int iValue1 = 0,iValue2 =0, iRet =0;
+
+    printf("Enter starting point : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter ending point : ");
+    scanf("%d",&iValue2);
+
+    iRet = RangeSumEven(iValue1,iValue2);
+
+    if (iRet == -1)
+    {
+        printf("Invalid Range");
+    }
+    else
+    {
+        printf("Addition is: %d",iRet);
+    }
+    
+
+    return 0;
+}
