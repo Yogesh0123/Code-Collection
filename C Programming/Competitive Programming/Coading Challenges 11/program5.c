926554b094b779635bb51458fda2f01ff763585a
Yogesh Anand Gadhave <111746500+Yogesh0123@users.noreply.github.com>
2026-06-16 23:35:14 +0530

    Uploded Competitive C Programming Files

---
+++ C Programming/Competitive Programming/Coading Challenges 11/program5.c
@@ -0,0 +1,44 @@
+/*
+5.Write a program which accept number from user and count frequency of such a
+digits which are less than 6.
+Input : 2395
+Output : 3
+Input : 1018
+Output : 3
+Input : 9440
+Output : 3
+Input : 96672
+Output : 1
+*/
+
+#include<stdio.h>
+int Count(int iNo)
+{
+    int iDigit =0;
+    int iCount =0;
+
+    while (iNo !=0)
+    {
+        iDigit = iNo % 10;
+        if (iDigit < 6 )
+        {
+            iCount++;
+        }
+        iNo = iNo /10;
+    }
+    return iCount;
+}
+int main()
+{
+int iValue = 0;
+int iRet = 0;
+
+printf("Enter number :");
+scanf("%d",&iValue);
+
+iRet = Count(iValue);
+
+printf("Output is : %d",iRet);
+
+return 0;
+}
