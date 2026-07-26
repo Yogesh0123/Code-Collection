dce0952ecb772ec121ce374637c15aa5cb693bd8
Yogesh Gadhave <yogeshgadhave356@gmail.com>
2026-07-26 20:08:43 +0530

    C++ Programming Practice Files Uploaded.

---
+++ C++ Programming/Practice Programs/program53.cpp
@@ -0,0 +1,55 @@
+/*
+Q. Accept number of rows and number of columns from user and display below
+pattern.
+Input : iRow = 4 iCol = 4
+Output : A B C D
+         a b c d
+         A B C D
+         a b c d
+*/
+
+#include<iostream>
+using namespace std;
+
+void Pattern(int iRow , int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+cout<<"Output is: \n";
+    for ( i = 1; i <= iRow; i++)
+    {
+        if (i % 2 != 0)
+        {
+            for ( j = 1; j <= iCol; j++)
+            {
+                cout<<char('A'+(j-1))<<"\t";
+            } 
+        }
+        else 
+        {
+             for ( j = 1; j <= iCol; j++)
+            {
+                cout<<char('a'+(j-1))<<"\t";
+            }
+        }
+
+        cout<<"\n";
+    }
+}
+
+int main()
+{
+    int iValue1=0;
+    int iValue2=0;
+
+    cout<<"Enter The Number of Row: ";
+    cin>>iValue1;
+
+    cout<<"Enter The Number of Columns: ";
+    cin>>iValue2;
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
