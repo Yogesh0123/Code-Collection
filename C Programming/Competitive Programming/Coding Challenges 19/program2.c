a70005a42c3673ce7b3f3a2e6998536d5e2e7b4a
Yogesh Gadhave <yogeshgadhave356@gmail.com>
2026-08-08 15:35:04 +0530

    C Programming Competitive Files Uploaded.

---
+++ C Programming/Competitive Programming/Coding Challenges 19/program2.c
@@ -0,0 +1,48 @@
+/*
+Q2.Accept Number of Rows and Number of columns from user and display below pattern.
+Input : iRow = 4 iCol = 4
+Output :* * * *
+        * * * #
+        * * # #
+        * # # #
+*/
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i=0;
+    int j=0;
+
+    for ( i = iRow; i >= 1; i--)
+    {
+        for ( j = 1; j <= iCol; j++)
+        {
+            if (j<=i)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("# ");
+            }
+            
+        }
+        printf("\n");
+    }
+    
+}
+
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter The Number of Row: ");
+    scanf("%d",&iValue1);
+
+    printf("Enter The Number of Columns: ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
