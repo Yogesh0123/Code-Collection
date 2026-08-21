c782694506f97f23bdceda7e4a10437997427353
Yogesh Gadhave <yogeshgadhave356@gmail.com>
2026-08-21 13:06:35 +0530

    C++ Programming Competitive Files Uploaded.

---
+++ C++ Programming/Competitive Programming/Coding Challenges 4/program2.cpp
@@ -0,0 +1,42 @@
+/*
+2. Accept number of rows and number of columns from user and display below pattern.
+Input : iRow = 4 iCol = 4
+Output :* * * *
+        * * *
+        * *
+        *
+*/
+
+#include<iostream>
+using namespace std;
+void Pattern(int iRow, int iCol)
+{
+    int i=iRow;
+    int j=0;
+
+    while (i>=1)
+    {
+        j=1;
+       while (j <= i)
+       {
+         cout<<"*\t";
+         j++;
+       }
+       cout<<"\n";
+       i--;
+    }
+}
+int main()
+{
+int iValue1 = 0;
+int iValue2 = 0;
+cout<<"Enter number of rows : ";
+cin>>iValue1;
+
+cout<<"Enter number of colums : ";
+cin>>iValue2;
+
+Pattern(iValue1, iValue2);
+
+return 0;
+}
