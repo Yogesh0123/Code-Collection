1dced63b04c564a843cdeb65a16b7750a02a3658
Yogesh Gadhave <yogeshgadhave356@gmail.com>
2026-08-25 19:13:31 +0530

    C++ Programming Competitive Files Uploaded.

---
+++ C++ Programming/Competitive Programming/Coding Challenges 5/program2.cpp
@@ -0,0 +1,21 @@
+// 2. Program to print 5 times "Marvellous" on screen.
+
+#include <iostream>
+using namespace std;
+
+void Display()
+{
+    int i = 0;
+
+    for (i = 1; i <= 5; i++)
+    {
+        cout << "Marvellous" << endl;
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
