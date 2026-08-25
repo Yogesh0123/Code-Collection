081246d99433a0433de81f0317214ca140b19a12
Yogesh Gadhave <yogeshgadhave356@gmail.com>
2026-08-25 19:17:41 +0530

    Java Programming Competitive Files Uploaded.

---
+++ Java Programming/Competitive Programming/Coding Challenges 8/program2.java
@@ -0,0 +1,19 @@
+// 2. Program to print 5 times "Marvellous" on screen.
+
+class Program2
+{
+    static void Display()
+    {
+        int i = 0;
+
+        for (i = 1; i <= 5; i++)
+        {
+            System.out.println("Marvellous");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Display();
+    }
+}
