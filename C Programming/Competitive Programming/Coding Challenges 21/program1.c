395d5e759044f90bfccb5a141cea2169ebaf42dd
Yogesh Gadhave <yogeshgadhave356@gmail.com>
2026-09-05 11:06:20 +0530

    C Programming Competitive Files Uploaded.

---
+++ C Programming/Competitive Programming/Coding Challenges 21/program1.c
@@ -0,0 +1,45 @@
+//Q1.Write a program which accept number from user and display its multiplication of factors.
+//Input : 12
+//Output : 144 (1 * 2 * 3 * 4 * 6)
+//Input : 13
+//Output : 1 (1)
+//Input : 10
+//Output : 10 (1 * 2 * 5)
+
+#include<stdio.h>
+
+int MultFact (int iNo)
+{
+int i = 0;
+int iMult = 1;
+
+if (iNo < 0)
+{
+    iNo = -iNo;
+}
+
+for ( i = 1; i <= iNo/2; i++)
+{
+    if (iNo % i ==0)
+    {
+        iMult = iMult * i;
+    }
+}
+return iMult;
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number ");
+    scanf ("%d",&iValue);
+
+    iRet = MultFact(iValue);
+
+    printf("Output is: %d",iRet);
+
+    return 0;
+}
---
+++ C Programming/Competitive Programming/Coding Challenges 21/program4.c
@@ -0,0 +1,46 @@
+//Q4.Write a program which accept number from user and return summation of all its non factors.
+//Input : 12
+//Output : 50
+//Input : 10
+//Output : 37
+
+#include<stdio.h>
+
+int SumNonFact(int iNo)
+
+{
+    int i =0;
+    int iSum =0;
+
+    if (iNo < 0)
+    {
+        iNo=-iNo;
+    }
+
+    for ( i = 1; i <= iNo; i++)
+    {
+        if (iNo % i !=0)
+        {
+            iSum = iSum + i;
+        }
+        
+    }
+    return iSum;
+    
+}
+
+int main()
+{
+    int iValue =0;
+    int iRet =0;
+
+    printf ("Enter Number : ");
+    scanf ("%d",&iValue);
+
+    iRet = SumNonFact(iValue);
+
+    printf("Output is : %d",iRet);
+
+    return 0;
+    
+}
